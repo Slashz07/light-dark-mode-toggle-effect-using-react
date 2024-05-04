3e488769a76408f0db0d1c0028275a04cdcd3784
Slashz07 <sumitforsaken76248@gmail.com>
2024-05-04 23:14:13 +0530

    toggle functionality created between light-dark mode using context-api

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,34 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import './App.css'
-import { ThemeProvider} from './contexts/Theme'
+import { ThemeProvider } from './contexts/Theme'
+import ThemeBtn from './components/ThemeBtn'
+import Card from './components/Card'
 function App() {
 
+  const [themeMode, setThemeMode] = useState("light")
+
+  useEffect(() => {
+    document.querySelector("html").classList.remove("dark","light")
+    document.querySelector("html").classList.add(themeMode)
+  }, [themeMode])
+
+  const darkTheme=()=> {
+    setThemeMode("dark")
+  }
+  const lightTheme=()=> {
+    setThemeMode("light")
+  }
   return (
     <>
       <ThemeProvider value={{themeMode,lightTheme,darkTheme}}>
 <div className="flex flex-wrap min-h-screen items-center">
                 <div className="w-full">
                     <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-                        
+                        <ThemeBtn/>
                     </div>
 
                     <div className="w-full max-w-sm mx-auto">
-                       
+                       <Card/>
                     </div>
                 </div>
             </div>
